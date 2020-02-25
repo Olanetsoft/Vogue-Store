6630ccfd4c883b93ef34720dc65d395f86595078
olanetsoft <heedris2olubisi@gmail.com>
2020-02-25 18:02:10 +0100

    BODY PARSER INSTALLED AND CODE CONVERTED TO EXPRESS IN NATURE

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the Middleware');
-    next();
+
+app.use('/add-product', (req, res, next)=>{
+    console.log('In the second Middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next)=>{
-    console.log('In the second Middleware');
+app.use('/product', (req, res, next)=> {
+
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next)=>{
     res.send('<h1>Hello from Express by Idris</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
