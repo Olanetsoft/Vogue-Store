1a032e5fcc29403ed9ce566e4f3959a5e5e57dd4
olanetsoft <heedris2olubisi@gmail.com>
2020-02-25 17:13:04 +0100

    EXPRESS WAS USED TO SEND RESPONSE TO THE BROWSER

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const http = require('http');
 
 const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer();
+app.use((req, res, next)=>{
+    console.log('In the Middleware');
+    next();
+})
+
+app.use((req, res, next)=>{
+    console.log('In the second Middleware');
+    res.send('<h1>Hello from Express by Idris</h1>')
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
