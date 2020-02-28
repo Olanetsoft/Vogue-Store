d483a7bce687944a2c433cf68f4ab5f89d6c183a
olanetsoft <heedris2olubisi@gmail.com>
2020-02-28 10:17:17 +0100

    VIEW FOLDER ADDED

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next)=>{
     console.log('In the second Middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-router.post('/product', (req, res, next)=> {
+router.post('/add-product', (req, res, next)=> {
     console.log(req.body);
     res.redirect('/');
 });
