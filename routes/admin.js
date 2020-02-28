4cd96de84859ea2c38e55b3bee12ee21716e38a6
olanetsoft <heedris2olubisi@gmail.com>
2020-02-28 12:21:18 +0100

    HELPER FUNCTION ADDED FOR GLOBAL DIRNAME

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const path = require('path');
-const express = require('express');
 
+const rootDir = require('../util/path');
+
+const express = require('express');
 const router = express.Router();
 
 
 router.get('/add-product', (req, res, next)=>{
     console.log('In the second Middleware');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add-product', (req, res, next)=> {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 const path = require('path');
-const express = require('express');
 
+const rootDir = require('../util/path');
+
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 
