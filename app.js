1b6b944d030211667d839bbc9866a6da78b96c10
olanetsoft <heedris2olubisi@gmail.com>
2020-02-28 13:40:17 +0100

    SYLE ENHANCED

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+ 
 
 
 app.use('/admin', adminRoutes);
