f1a82e0a4ad758d37e14e6907fd1cd4d3cd753cf
olanetsoft <heedris2olubisi@gmail.com>
2020-02-28 13:24:09 +0100

    SERVING STYLE STATICALLY

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
