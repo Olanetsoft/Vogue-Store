aab6b230bf028679a9145ff74a45fc4c1ea5aefb
olanetsoft <heedris2olubisi@gmail.com>
2020-02-28 11:45:25 +0100

    ADDED SHOP AND ADD-PRODUCT PAGE

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +6,7 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next)=>{
     console.log('In the second Middleware');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname, './', 'views', 'add-product.html'));
 })
 
 router.post('/add-product', (req, res, next)=> {
