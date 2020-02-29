712320a09b344fd3cc92c81fcba6574af5098a06
olanetsoft <heedris2olubisi@gmail.com>
2020-02-29 17:15:28 +0100

    UPDATED SHOP PAGE WITH GLODAL LAYOUT CONCEPT

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const adminData = require('./admin')
 
 router.get('/', (req, res, next)=>{
     const products = adminData.products
-    res.render('shop', {prods: products, shopTitle: 'My shop'});
+    res.render('shop', {prods: products, pageTitle: 'My shop', path: '/'});
 });
 
 
