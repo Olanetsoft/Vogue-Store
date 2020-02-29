18ac428306dcf107e40d3684c2114e7cb4d70485
olanetsoft <heedris2olubisi@gmail.com>
2020-02-29 13:47:40 +0100

    SHOP.PUG page UPDATED AND COMPLETED THE MODULE USING PUG TEMPLATING ENGINES

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,8 @@ const router = express.Router();
 const adminData = require('./admin')
 
 router.get('/', (req, res, next)=>{
-    res.render('shop');
+    const products = adminData.products
+    res.render('shop', {prods: products, shopTitle: 'My shop'});
 });
 
 
