d03d8ce446940ec786e88d135b86fa405972ba0d
olanetsoft <heedris2olubisi@gmail.com>
2020-03-07 20:27:59 +0100

    RESTORED UTIL FOLDER, ADDED STORE PROCEDURE TO A FILE AND FETCH PROCEDURE

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,30 @@
-const ImportProduct = require('../models/product');
+const Product = require('../models/product');
 
-exports.getAddedProduct = (req, res, next)=>{
-    console.log('In the products.js');
-    res.render('add-product',
-    {pageTitle: 'Add Product',
-    path: '/admin/add-product'})
+exports.getAddedProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 };
 
-exports.postAddedProduct = (req, res, next)=> {
-    const product = new ImportProduct(req.body.title);
-    product.save();
-    res.redirect('/');
-}
+exports.postAddedProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
-exports.getProducts = (req, res, next)=>{
-    const products = ImportProduct.fetchAll();
-    res.render('shop', 
-    {prods: products, 
-    pageTitle: 'My shop', 
-    path: '/'});
-}
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,15 +1,37 @@
-const products = [];
-module.exports = class Product{
-    constructor(t){
-        this.title = t
-    }
+const fs = require('fs');
+const path = require('path');
 
-    //save the product into the product array
-    save(){
-        products.push(this);
-    }
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-    static fetchAll() {
-        return products;
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-}
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
