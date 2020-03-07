8c3dc5e1d9f83208b2f18097a83bba384fd2bbcc
olanetsoft <heedris2olubisi@gmail.com>
2020-03-07 21:24:41 +0100

    FIXED 404 PAGE ERROR '404 NOT FOUND'

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404Page = (req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Error !'})
-};
+    res.status(404).render('404', {pageTitle: 'Error !', path: '404'})
+};  
