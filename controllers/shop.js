27f9a3b87bd9349a179133d4a20a82af10dd2ae9
olanetsoft <heedris2olubisi@gmail.com>
2020-03-08 13:36:41 +0100

    ADDED ODERS.EJS

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,9 +27,16 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout'
   });
-}
+}; 
