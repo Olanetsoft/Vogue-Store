9e01bcc33d7ffc3850b9d867f778eb70c7230d50
olanetsoft <heedris2olubisi@gmail.com>
2020-03-11 20:58:26 +0100

    CART.JS MODEL CREATED

---
+++ models/cart.js
@@ -0,0 +1,8 @@
+
+module.exports = class Cart {
+   static addProduct(id){
+       //fetch the previous cart
+       //Analyze the cat => find existing product
+       //add new product / increase quantity
+   }
+}
