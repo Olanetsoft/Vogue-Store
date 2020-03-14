b1859ef42171bfe23bc9de35a5fb720ad54486c5
olanetsoft <heedris2olubisi@gmail.com>
2020-03-14 17:23:20 +0100

    DATA SAVED TO DATABASE SUCCESSFULLY

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,71 +8,76 @@ exports.getAddedProduct = (req, res, next) => {
   });
 };
 
-  
-  exports.postAddedProduct = (req, res, next) => {
-    //this section below get the request details from the form
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    
 
-    const product = new Product(null, title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
-  };
+exports.postAddedProduct = (req, res, next) => {
+  //this section below get the request details from the form
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
 
-  exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
+
+  const product = new Product(null, title, imageUrl, description, price);
+  product
+    .save()
+    .then(()=>{
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findProductById(prodId, product => {
+    if (!product) {
       return res.redirect('/');
     }
-    const prodId = req.params.productId;
-    Product.findProductById(prodId, product => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode, 
-        product: product
-      });
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
     });
-  };
+  });
+};
 
 
-  exports.postEditProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-      prodId,
-      updatedTitle,
-      updatedImageUrl,
-      updatedDesc,
-      updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect('/admin/products-list')
-  };
-    
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products-list')
+};
 
-  exports.getProductsList = (req, res, next) =>{
-    Product.fetchAll(products => {
-      res.render('admin/products-list', {
-        prods: products,
-        pageTitle: 'Admin Products', 
-        path: 'admin/products-list'
-      });
+
+exports.getProductsList = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products-list', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: 'admin/products-list'
     });
-  };
+  });
+};
 
 
-  exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products-list');
-  }
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products-list');
+}
--- models/product.js
+++ models/product.js
@@ -11,7 +11,9 @@ module.exports = class Product {
   }
 
   save() {
-  
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+    [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
   static deleteById(id){
