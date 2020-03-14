31c056ab4d9b46b8e88379384733155e77010d3a
olanetsoft <heedris2olubisi@gmail.com>
2020-03-14 14:26:15 +0100

    ADD TO CART COMPLETED

--- models/cart.js
+++ models/cart.js
@@ -61,4 +61,15 @@ module.exports = class Cart {
       });
     });
   }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };
