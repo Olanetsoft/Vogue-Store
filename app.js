7fc7bade9e907342e2a1345f90efee5d3fde4c99
olanetsoft <heedris2olubisi@gmail.com>
2020-03-16 17:13:22 +0100

    SEQUELIZE CODE  REMOVE AND MONGODB INSTALLED

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ const bodyParser = require('body-parser');
 //Importing the error controller
 const errorController = require('./controllers/error');
 
-const sequelize = require('./util/database');
-
-//Importing models of product,user,cart,cart Item,order,order Item
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
-
 const app = express();
 
 //set this value globally in our application
@@ -35,12 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Retrieving user by Id and it only runs for incoming request
 app.use((req, res, next) =>{
-    User.findById(1)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => console.log(err))
+    // User.findById(1)
+    // .then(user => {
+    //     req.user = user;
+    //     next();
+    // })
+    // .catch(err => console.log(err))
 });
 
 
@@ -51,44 +40,4 @@ app.use(shopRoutes);
 
 //This section below returns the default 404page when a path that doesn't exist is hit
 app.use(errorController.get404Page);
-
-//This add relations between users and product
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-//This is optional but can also be stated because a user can have many product
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart,  {through: CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product,  {through: OrderItem});
-
-
-
-//this sync model to database by creating a table if it does not exist
-sequelize
-  //.sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findById(1);
-    // console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Idris', email: 'test@test.com' });
-    }
-    return user;
-  })
-  .then(user => {
-    //console.log(user);
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
 
--- util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('learn-node', 'root', 'Hayindeh2019@', 
-{dialect: 'mysql', host: 'localhost'}
-);
-
-module.exports = sequelize;
