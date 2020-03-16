039dc3f0ddc50b45651d6d63b171fd72bd89dbe4
olanetsoft <heedris2olubisi@gmail.com>
2020-03-16 12:52:24 +0100

    CREATED ORDER MODEL

---
+++ models/order.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Order = sequelize.define('order', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  quantity: Sequelize.INTEGER
+});
+
+module.exports = Order;
