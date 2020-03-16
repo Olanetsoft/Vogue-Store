a8c56af65c1de6c5c16d49d7868080b897624c85
olanetsoft <heedris2olubisi@gmail.com>
2020-03-16 12:40:14 +0100

    COMPLETED ADDING OF NEW CART ITEM AND ALSO POPULATING OLD ONESS

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,7 +59,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById({prodId})
+  Product.findById(prodId)
   .then(product => {
     product.title = updatedTitle;
     product.price = updatedPrice;
--- controllers/shop.js
+++ controllers/shop.js
@@ -68,6 +68,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
   .getCart()
   .then(cart => {
@@ -79,17 +80,20 @@ exports.postCart = (req, res, next) => {
       {
         product = products[0];
       }
-
-      let newQuantity = 1;
+      
       if(product){
-
+        //if product exist
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
       return Product.findById(prodId)
-      .then(product =>{
-        return fetchedCart.addProduct(product, { through: {quantity: newQuantity} });
-      })
-      .catch(err => console.log(err))
-    }).then( () => {
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, 
+        { through: {quantity: newQuantity} });
+    })
+    .then( () => {
       res.redirect('/cart');
     } )
     .catch(err => console.log(err))
