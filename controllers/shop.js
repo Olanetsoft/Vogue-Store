89d8c34f93a4e150511794bad1802fbc8aea372f
olanetsoft <heedris2olubisi@gmail.com>
2020-03-19 15:54:15 +0100

    POPULATE CART PAGE COMPLETED

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,9 +45,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
-
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProductById);
 
-// router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
