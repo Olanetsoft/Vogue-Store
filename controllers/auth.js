c858d12a8fee5157bc52e91bf5b126dd18f1eb9a
olanetsoft <heedris2olubisi@gmail.com>
2020-03-21 12:36:41 +0100

    SIGN IN AND SIGN UP WORKS FINE

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,24 +15,40 @@ exports.getLogin = (req, res, next) => {
 
 
 exports.PostLogin = (req, res, next) => {
-    User.findById('5e75db0b95725d2b884cf35d')
-    .then(user => {
-        req.session.isLoggedIn = true
-        req.session.user = user;
-        req.session.save(err => {
-            res.redirect('/');
+    const extractedEmail = req.body.email;
+    const extractedPassword = req.body.password;
+
+    User.findOne({ email: extractedEmail })
+        .then(user => {
+            if (!user) {
+                return res.redirect('/login')
+            }
+            bcrypt.compare(extractedPassword, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login')
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.redirect('/login')
+                });
         })
-    })
-    .catch(err => console.log(err));
-    
+        .catch(err => console.log(err));
+
 };
 
-exports.getSignup = (req, res, next) =>{
+exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
         isAuthenticated: false
-      });
+    });
 };
 
 
@@ -47,25 +63,25 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
 
-    User.findOne({email: email})
-    .then(userDoc => {
-        if(userDoc){
-            return res.redirect('/signup')
-        }
-        return bcrypt.hash(password, 12)
-        .then(hashedPassword => {
-            const user = new User({
-                email: email,
-                password: hashedPassword,
-                cart: {items: []}
-            });
-            return user.save();
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup')
+            }
+            return bcrypt.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
+                })
         })
-    })
-    .then(result => {
-        res.redirect('/login')
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(result => {
+            res.redirect('/login')
+        })
+        .catch(err => {
+            console.log(err)
+        })
 };
