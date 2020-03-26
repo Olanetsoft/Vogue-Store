735fbf09d86cd2b378f8988142829e263387bdbd
olanetsoft <heedris2olubisi@gmail.com>
2020-03-26 12:30:21 +0100

    USING  ASYNC JAVASCRIPT TO DELETE AN ITEM WITHOUT PAGE RELOAD COMPLETED

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,13 +3,20 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+    //getting the card on the DOM
+    const productElement = btn.closest('article');
+
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
         headers: {
             'csrf-token': csrf
         }
-    }).then(result => {
-        console.log(result);
+    })
+    .then(result => {
+        return result.json();
+    })
+    .then(data => {
+       productElement.parentNode.removeChild(productElement);
     })
     .catch(err => {
         console.log(err);
